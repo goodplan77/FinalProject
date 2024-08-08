cdaea4170f8f056ba1f379c9821c7af79c640b9d
parkjh1000 <pjh0115@naver.com>
2024-08-08 12:16:49 +0900

    DB 컨펌 후 TABLE_SCRIPT 수정, VO클래스 삭제 및 수정

--- backend/src/main/java/com/kh/backend/model/vo/Alarm.java
+++ backend/src/main/java/com/kh/backend/model/vo/Alarm.java
@@ -13,11 +13,9 @@ public class Alarm {
 	private int userNo;
 	private int fromUserNo;
 	private String content;
-	private char status;
+	private char status; // "N - 안읽음 Y - 읽음"
 	private String alarmDate; // 일단 String
-	private char alarmType; // "C - 댓글 (BOARD) L - 좋아요 (BOARD) R - 대댓글 (COMMENT)	M - 채팅메세지 (CHAT_ROOM)"
-	private int chatRoomNo;
-	private int boardNo;
-	private int commentNo;
+	private char typeCode; // B, C, L, M, R, P
+	private int refNo;
 
 }
