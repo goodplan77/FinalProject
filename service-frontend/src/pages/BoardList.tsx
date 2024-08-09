9ffe24e2334fb1517233973c8b5c67b0e84dad52
hijinho3 <hijinho3@naver.com>
2024-08-09 17:21:56 +0900

    0809 1721

--- service-frontend/src/pages/BoardList.tsx
+++ service-frontend/src/pages/BoardList.tsx
@@ -27,7 +27,10 @@ export default function BoardList() {
     useEffect(() => {
         axios.get("http://localhost:8013/banju/board/boardList")
             .then((response) => {
+                console.log(response);
                 dispatch(selectAllBoard(response.data));
+            }).catch((response) => {
+                console.log(response);
             })
     }, [])
     return (
@@ -55,103 +58,64 @@ export default function BoardList() {
             {
                 boards.map((board) => {
                     return (
-                        <>
+                        <div>
                             {/* 보드 카테고리바 아래에 보드 리스트입니다. */}
                             <div className={styles.classic}>
-                                <div className={styles.classicTitle}>
-                                    <div className={styles.classicContents}>
-                                        <div className={styles.classicContent}>
-                                            <div className={styles.img}></div>
-                                            <div className={styles.upAndDown}>
-                                                <div className={styles.contentUp}>
-                                                    <div className={styles.contentTitle}>
-                                                        <p>{board.title}</p>
-                                                    </div>
-                                                    <div className={styles.upRight}>
-                                                        <div className={styles.contentInfo}>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/view.png`} alt="view" />
-                                                            <p className={styles.viewNo}>{board.views}</p>
-                                                        </div>
-                                                        <p className={styles.contentDate}>{board.enrollDate}</p>
-                                                    </div>
+                                <div className={styles.classicContent} key={board.boardNo}>
+                                    <div className={styles.img}></div>
+                                    <div className={styles.upAndDown}>
+                                        <div className={styles.contentUp}>
+                                            <div className={styles.contentTitle}>
+                                                <p>{board.title}</p>
+                                            </div>
+                                            <div className={styles.upRight}>
+                                                <div className={styles.contentInfo}>
+                                                    <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/view.png`} alt="view" />
+                                                    <p className={styles.viewNo}>{board.views}</p>
                                                 </div>
-
-                                                <div className={styles.contentDown}>
-                                                    <p className={styles.downNick}>닉네임닉네임</p>
-                                                    <div className={styles.downRight}>
-                                                        <div className={styles.emptyPlace}></div>
-                                                        <div className={styles.contentLike}>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/like.png`} alt="like" />
-                                                            <p>20</p>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/comment.png`} alt="comment" />
-                                                            <p>18</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                                <p className={styles.contentDate}>{board.enrollDate}</p>
                                             </div>
                                         </div>
 
-
-
-                                        <div className={styles.classicContent}>
-                                            <div className={styles.upAndDown}>
-                                                <div className={styles.contentUp}>
-                                                    <div className={`${styles.contentTitle}`} style={{ marginLeft: "5px" }}>
-                                                        <p>사진이 없을때1</p>
-                                                    </div>
-                                                    <div className={`${styles.upRight}`} style={{ marginLeft: "100px" }}>
-                                                        <div className={styles.contentInfo}>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/view.png`} alt="view" />
-                                                            <p className={styles.viewNo}>20</p>
-                                                        </div>
-                                                        <p className={styles.contentDate}>2024-01-01</p>
-                                                    </div>
-                                                </div>
-
-                                                <div className={styles.contentDown}>
-                                                    <p className={styles.downNick} style={{ marginLeft: "5px" }}>닉네임닉네임</p>
-                                                    <div className={styles.downRight}>
-                                                        <div className={styles.emptyPlace}></div>
-                                                        <div className={styles.contentLike} style={{ marginLeft: "90px" }}>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/like.png`} alt="like" />
-                                                            <p>20</p>
-                                                            <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/comment.png`} alt="comment" />
-                                                            <p>18</p>
-                                                        </div>
-                                                    </div>
+                                        <div className={styles.contentDown}>
+                                            <p className={styles.downNick}>닉네임닉네임</p>
+                                            <div className={styles.downRight}>
+                                                <div className={styles.emptyPlace}></div>
+                                                <div className={styles.contentLike}>
+                                                    <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/like.png`} alt="like" />
+                                                    <p>20</p>
+                                                    <img className={styles.view} src={`${process.env.PUBLIC_URL}/images/comment.png`} alt="comment" />
+                                                    <p>18</p>
                                                 </div>
                                             </div>
-                                        </div>
-
-
-
-                                        {/* isnertBoard 페이지로 이동하는 버튼(fixed) */}
-                                        <div className='plus' style={{
-                                            backgroundColor: "#02CCFE",
-                                            width: "70px",
-                                            height: "70px",
-                                            borderRadius: "35px",
-                                            opacity: "50%",
-                                            position: "fixed",
-                                            bottom: "70px",
-                                            right: "10px",
-                                        }} onClick={() => navi('/insertBoard')}>
-                                            <img className='plus-pen' src={`${process.env.PUBLIC_URL}/images/pen.png`} alt="글쓰기" style={{
-                                                marginTop: "9px",
-                                                marginLeft: "9px",
-                                                width: "50px",
-                                                height: "50px",
-                                            }} />
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </>
+                        </div>
                     )
                 })
 
 
             }
+            {/* isnertBoard 페이지로 이동하는 버튼(fixed) */}
+            <div className='plus' style={{
+                backgroundColor: "#02CCFE",
+                width: "70px",
+                height: "70px",
+                borderRadius: "35px",
+                opacity: "50%",
+                position: "fixed",
+                bottom: "70px",
+                right: "10px",
+            }} onClick={() => navi('/insertBoard')}>
+                <img className='plus-pen' src={`${process.env.PUBLIC_URL}/images/pen.png`} alt="글쓰기" style={{
+                    marginTop: "9px",
+                    marginLeft: "9px",
+                    width: "50px",
+                    height: "50px",
+                }} />
+            </div>
         </>
     );
 }
