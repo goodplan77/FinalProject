ac14bb9b23bdd9d044d6303f90dbafab9f7302ac
Inseak <dskaljdlfa@gmail.com>
2024-08-20 12:04:39 +0900

    문의 모달 / 문의 insert / 햄버거 바 게시글 목록 모달 / 중복된 메인페이지, 마이페이지 헤더 푸터 정리

--- service-frontend/src/components/Navibar.tsx
+++ service-frontend/src/components/Navibar.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Navibar.module.css';
-export default function Navibar(){
 
+export default function Navibar() {
     const navi = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
 
-    return(
+    const handleHamburgerClick = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setIsMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isMenuOpen]);
+
+
+    return (
         <div className={styles.container}>
-            <img src='/images/home.png' alt="메인페이지로" style={{height : "30px"}} onClick={()=>navi('/')}/>
-            <img src='/images/hamburger.png' alt='게시판 목록' style={{height : "30px"}}/>
-            <img src='/images/message.png' alt='메세지' style={{height : "28px"}}/>
-            <img src='/images/login.png' alt='마이페이지' style={{height : "32px"}} onClick={()=>navi('signup')}/>
+            <img src='/images/home.png' alt="메인페이지로" style={{ height: "30px" }} onClick={() => navi('/')} />
+            <img src='/images/hamburger.png' alt='게시판 목록' style={{ height: "30px" }} onClick={handleHamburgerClick} />
+            <img src='/images/message.png' alt='메세지' style={{ height: "28px" }} onClick={() => navi('chatList')} />
+            <img src='/images/login.png' alt='마이페이지' style={{ height: "32px" }} onClick={() => navi('mypage')} />
+
+            {isMenuOpen && (
+                <div className={styles.menu} ref={menuRef}>
+                    <ul>
+                        <li onClick={() => navi('boardList')}>일반 게시판</li>
+                        <li onClick={() => navi('usedList')}>중고 거래</li>
+                        <li onClick={() => navi('adoptList')}>입양</li>
+                        <li onClick={() => navi('missingList')}>실종 신고</li>
+                        <li onClick={() => navi('dogInfo')}>강아지 정보</li>
+                        <li onClick={() => navi('weather')}>날씨</li>
+                        <li onClick={() => navi('petPlace')}>펫 플레이스</li>
+                        <li onClick={() => navi('event')}>이벤트</li>
+                    </ul>
+                </div>
+            )}
         </div>
-    )
+    );
 }
