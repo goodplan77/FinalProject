896e63a8944f5dadd032c0771bcff688450e752c
parkjh1000 <pjh0115@naver.com>
2024-08-26 18:10:06 +0900

    새로고침시 store 유지됨

--- service-frontend/src/index.tsx
+++ service-frontend/src/index.tsx
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,9 +17,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
 );
 
--- service-frontend/src/store/store.ts
+++ service-frontend/src/store/store.ts
@@ -1,16 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import boardSlice from "../features/boardSlice";
 import memoSlice from "../features/memoSlice";
 import userSlice from "../features/userSlice";
 import modalSlice from "../features/modalSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({
+    boards: boardSlice,
+    memos: memoSlice,
+    user : userSlice,
+    modal : modalSlice
+})
+
+const persistConfig = {
+    key : "root",
+    storage,
+    witelist : ["user"]
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-    reducer: {
-        boards: boardSlice,
-        memos: memoSlice,
-        user : userSlice,
-        modal : modalSlice
-    }
+    reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
